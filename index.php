c29ecd54018f6843a38cc63fcd31d248bbb5bade
aromero7764 <aromero7764@gmail.com>
2022-06-10 19:22:01 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alberto Romero</title>
+    <link rel="stylesheet" href="css/bulma.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+<body>
+<?php include("template/cabecera.php") ?> 
+    <header class="header content">
+      <div class="header-video">
+        <video src="videos/video1.mp4" autoplay="true" muted="true" loop="true" "> </video>
+      </div>
+      <div class="header-overlay"></div>
+      <div class="header-content">
+        <h1>Alberto Romero</h1>
+        <p>The best way to predict the future is to invent it</p>
+      </div>
+    </header>
+    
+    <article class="panel is-primary">
+  <p class="panel-heading">
+    Primary
+  </p>
+  <p class="panel-tabs">
+    <a class="is-active">All</a>
+    <a>Public</a>
+    <a>Private</a>
+    <a>Sources</a>
+    <a>Forks</a>
+  </p>
+  <div class="panel-block">
+    <p class="control has-icons-left">
+      <input class="input is-primary" type="text" placeholder="Search">
+      <span class="icon is-left">
+        <i class="fas fa-search" aria-hidden="true"></i>
+      </span>
+    </p>
+  </div>
+  <a class="panel-block is-active">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    bulma
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    marksheet
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    minireset.css
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    jgthms.github.io
+  </a>
+</article>
---
+++ template/cabecera.php
@@ -0,0 +1,52 @@
+<?php $url="http://".$_SERVER['HTTP_HOST']."/sitio4"  ?>
+<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+  <div class="header-content">
+  <h1>Alberto Romero</h1>
+  <div class="navbar-end">
+  <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+  <span aria-hidden="false"></span>
+  <span aria-hidden="false"></span>
+  <span aria-hidden="false"></span>
+</a>
+  </div>
+  
+
+<script src="../js/menu.js"></script>
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="<?php echo $url."../index.php"; ?>">
+        Home
+      </a>
+
+      <a class="navbar-item">
+        Proyectos
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Más
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+          Acerca de mi
+          </a>
+          <a class="navbar-item">
+          Curriculum
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Contacto
+          </a>
+        </div>
+        <a class="navbar-item">
+      </a>
+
+      </div>
+    </div>
+  </div>
+</div>
+<div>
+  </div>
+</div>
+</nav>
